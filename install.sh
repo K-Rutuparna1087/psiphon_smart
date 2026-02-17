c3041df7cd52225334badbd211f693797d18cfb7
Kulkarni Rutuparna Ratnakar <151852360+K-Rutuparna1087@users.noreply.github.com>
2026-02-17 10:01:05 +0530

    Modify install.sh for Psiphon installation process
    Updated installation script to install Psiphon and set up executable path.

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+echo "Installing Psiphon Package"
+sudo apt update
+sudo apt install wget -y
+cd ~/Downloads
+wget https://raw.githubusercontent.com/SpherionOS/PsiphonLinux/main/plinstaller2
+sudo sh plinstaller2
+sudo rm -rf plinstaller2
 
 echo "Installing Psiphon Smart Launcher..."
-
-sudo apt update
 sudo apt install wget curl netcat-openbsd -y
 
 wget -O ~/psiphon_smart.sh https://raw.githubusercontent.com/K-Rutuparna1087/psiphon_smart/main/psiphon_smart.sh
 chmod +x ~/psiphon_smart.sh
 
-echo '#!/bin/bash' | sudo tee /usr/local/bin/psiphon > /dev/null
-echo 'bash ~/psiphon_smart.sh' | sudo tee -a /usr/local/bin/psiphon > /dev/null
+echo '#!/bin/bash' | sudo tee /usr/bin/psiphon > /dev/null
+echo 'bash ~/psiphon_smart.sh' | sudo tee -a /usr/bin/psiphon > /dev/null
 sudo chmod +x /usr/local/bin/psiphon
 
 echo "Installation complete. Run: psiphon"
